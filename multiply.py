3503405bffdeb9ed299a492d7971ed4bfe7a004c
Mubashir Hussain <kingmoby99@gmail.com>
2024-04-16 21:17:54 +0500

    added multiply.py

---
+++ multiply.py
@@ -0,0 +1,9 @@
+# Define the numbers
+num1 = 5
+num2 = 10
+
+# Calculate the product
+product_result = num1 * num2
+
+# Print the product (optional)
+print("The product of", num1, "and", num2, "is:", product_result)
