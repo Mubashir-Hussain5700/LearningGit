105ce35687ec5fbf2ac85bb78f6b17c1c6ba014c
Mubashir Hussain <kingmoby99@gmail.com>
2024-04-16 22:40:49 +0500

    added implementation for diff.py

---
+++ diff.py
@@ -0,0 +1,9 @@
+# Define the numbers
+num1 = 20
+num2 = 7
+
+# Calculate the difference
+difference_result = num1 - num2
+
+# Print the difference (optional)
+print("The difference between", num1, "and", num2, "is:", difference_result)
